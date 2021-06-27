a9001a67af40b24b5b066311e41ac820b4b83c59
Aytaj Mahammadli <aytajmahammadli@gmail.com>
2021-06-28 03:53:42 +0400

    some changes on next button

--- assets/index.js
+++ assets/index.js
@@ -1,44 +1,62 @@
-function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
+function togglePopup() {
+  document.getElementById("popup-1").classList.toggle("active");
 }
 
 function openSection(event, tabName) {
-    var tabContent = document.getElementsByClassName("contentText");
-    for(var i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
-    }
-    var tabLinks = document.getElementsByClassName("tablinks");
-    for (var i = 0; i < tabLinks.length; i++) {
-        // string.replace(originalstr,newstr)  
-      tabLinks[i].className = tabLinks[i].className.replace("active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    event.currentTarget.className += " active";
+  var tabContent = document.getElementsByClassName("contentText");
+
+  var tabLinks = document.getElementsByClassName("tablinks");
+  for (var i = 0; i < tabLinks.length; i++) {
+    tabContent[i].classList.remove("active");
+    tabLinks[i].className = tabLinks[i].className.replace("active", "");
+    document.getElementById(tabName).classList.add("active");
   }
 
+  event.currentTarget.className += " active";
 
-  // function displayCurrentTab(event, tabName){
-  //   var tabContent = document.getElementsByClassName("contentText");
-  //   for(var i = 0; i < tabContent.length; i++) {
-  //       if(tabContent[i].style.display = "block"){
-  //           tabContent[i].style.display = "none";
-  //           tabContent[i] = tabContent[i + 1];
-  //           tabContent[i].style.display = "block";
-  //       }
-  //     }
-  //   var tabLinks = document.getElementsByClassName("tablinks");
-  //   for (var i = 0; i < tabLinks.length; i++) {
-  //       if(tabLinks[i].classList.contains("active")){
-  //           console.log(tabLinks[i]);
-  //           tabLinks[i].className = tabLinks[i].className.replace("active", "");
-  //           tabLinks[i] = tabLinks[i + 1];
-  //           console.log(tabLinks[i+1]);
-  //           event.currentTarget.className += " active";
-  //       }
-  //   }
-  // }
+  document.getElementById("next").style.pointerEvents = "initial";
+}
 
-  // var nextButton = document.getElementById("next");
+function displayCurrentTab(event, tabName) {
+  var tabContent = document.getElementsByClassName("contentText");
 
-  // nextButton.addEventListener("click", displayCurrentTab);
+  for (var i = 0; i < tabContent.length; i++) {
+    if (tabContent[i].classList.contains("active")) {
+      if (
+        tabContent[i].classList.contains("active") &&
+        tabContent[i].classList.contains("contentText-5")
+      ) {
+        nextButton.style.pointerEvents = "none";
+      } else {
+        nextButton.style.pointerEvents = "initial";
+        tabContent[i + 1].classList.add("active");
+        tabContent[i].classList.remove("active");
+        return;
+      }
+    }
+  }
+}
 
+function dispTab() {
+  var tablink = document.getElementsByClassName("tablinks");
+  for (var j = 0; j < tablink.length; j++) {
+    if (tablink[j].classList.contains("active")) {
+      if (
+        tablink[j].classList.contains("active") &&
+        tablink[j].classList.contains("disabled")
+      ) {
+        nextButton.style.pointerEvents = "none";
+      } else {
+        nextButton.style.pointerEvents = "initial";
+        tablink[j + 1].classList.add("active");
+        tablink[j].classList.remove("active");
+        return;
+      }
+    }
+  }
+}
+
+var nextButton = document.getElementById("next");
+
+nextButton.addEventListener("click", displayCurrentTab);
+nextButton.addEventListener("click", dispTab);
